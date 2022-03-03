7698e0911db8937e347ff3033c1cf6c84b5b9269
Nguyen Sinh Hien <19021269@vnu.edu.vn>
2022-03-03 22:13:51 +0700

    lesson 10

---
+++ lesson10/lib/main.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/material.dart';
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo', theme: ThemeData(
+      primarySwatch: Colors.red,),
+      home: MyHomePage(title: 'Product layout demo home page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key? key,required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title:Text("Hello")),
+        body: ListView(
+          shrinkWrap: true, padding: const EdgeInsets.fromLTRB(2.0, 10.0, 2.0, 10.0),
+          children: <Widget> [
+            ProductBox(
+                name: "IU",
+                description: "IU 1",
+                price: 28,
+                image: "iu.jpeg"
+            ),
+            ProductBox(
+                name: "IU",
+                description: "IU 2",
+                price: 28,
+                image: "iu.jpeg"
+            ),
+            ProductBox(
+                name: "IU",
+                description: "IU 3",
+                price: 28,
+                image: "iu.jpeg"
+            ),
+            ProductBox(
+                name: "IU",
+                description: "IU 4",
+                price: 28,
+                image: "iu.jpeg"
+            ),
+            ProductBox(
+                name: "IU",
+                description: "IU 5",
+                price: 28,
+                image: "iu.jpeg"
+            ),
+            ProductBox(
+                name: "IU",
+                description: "IU 6",
+                price: 28,
+                image: "iu.jpeg"
+            ),
+          ],
+        )
+    );
+  }
+}
+class RatingBox extends StatefulWidget {
+  @override
+  _RatingBoxState createState() =>
+      _RatingBoxState();
+}
+class _RatingBoxState extends State<RatingBox> {
+  int _rating = 0;
+  void _setRatingAsOne() {
+    setState( () {
+      _rating = 1;
+    });
+  }
+  void _setRatingAsTwo() {
+    setState( () {
+      _rating = 2;
+    });
+  }
+  void _setRatingAsThree() {
+    setState( () {
+      _rating = 3;
+    });
+  }
+  Widget build(BuildContext context) {
+    double _size = 20;
+    print(_rating);
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      mainAxisSize: MainAxisSize.max,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_rating >= 1 ? Icon(Icons.star, size: _size,) :
+            Icon(Icons.star_border, size: _size,)),
+            color: Colors.red[500],
+            onPressed: _setRatingAsOne,
+            iconSize: _size,
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_rating >= 2 ? Icon(Icons.star, size: _size,) :
+            Icon(Icons.star_border, size: _size,)),
+            color: Colors.red[500],
+            onPressed: _setRatingAsTwo,
+            iconSize: _size,
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_rating >= 3 ? Icon(Icons.star, size: _size,) :
+            Icon(Icons.star_border, size: _size,)),
+            color: Colors.red[500],
+            onPressed: _setRatingAsThree,
+            iconSize: _size,
+          ),
+        ),
+      ],
+    );
+  }
+}
+class ProductBox extends StatelessWidget {
+  ProductBox({Key? key, required this.name, required this.description, required this.price, required this.image}) :
+        super(key: key);
+  final String name;
+  final String description;
+  final int price;
+  final String image;
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.all(2),
+        height: 140,
+        child: Card(
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Image.asset("assets/" + image),
+                  Expanded(
+                      child: Container(
+                          padding: EdgeInsets.all(5),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              Text(this.name, style: TextStyle(fontWeight: FontWeight.bold)),
+                              Text(this.description),
+                              Text("Age: " + this.price.toString()),
+                              RatingBox(),
+                            ],
+                          )
+                      )
+                  )
+                ]
+            )
+        )
+    );
+  }
+}
